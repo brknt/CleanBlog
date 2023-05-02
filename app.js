6f85811999efb468abb12676b416da881cf0636b
brknt <brknt.gns@hotmail.com>
2023-05-02 14:06:38 +0300

    updated file

--- app.js
+++ app.js
@@ -11,3 +11,20 @@
 // .gitignore dosyası oluşturalım ve ilk repomuzu gönderelim.
 
 
+const express = require('express');
+
+const app = express();
+
+const blog = { id: 1, title: "Blog title", description: "Blog description" };
+
+app.get('/',(req,res) =>{
+    console.log('get çağrıldı.');
+    res.send(blog);
+});
+
+
+const port = 3000;
+app.listen(port,() => {
+    console.log(`Server ${port} portunda başlatıldı...`);
+    
+});
